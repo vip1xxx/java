512fa4675a7460419a06045dff42faa3bb9b2e22
x <fedorenkomcbook@gmail.com>
2023-08-16 16:25:11 +0200

    init commit

---
+++ lesson 1/main.js
@@ -0,0 +1,24 @@
+let a1 = 'hello'; let a2 = "owu"; let a3 ="com"; let a4 = "ua";
+console.log(a1); console.log(a2); console.log(a3); console.log(a4);
+
+let n1 = 1; let n2 = 10; let n3 = -999; let n4 = 123; let n5 = 3.14; let n6 = 2.7; let n7 = 16;
+console.log(n1); console.log(n2); console.log(n3); console.log(n4); console.log(n5); console.log(n6); console.log(n7);
+
+let t = true; let f = false;
+console.log(t); console.log(f);
+
+let firstName = ' Oleksandr '; let middleName = 'Fedorenko '; let lastName = 'Serhiyovych ';
+let fullName = middleName + "" + firstName + "" + lastName;
+console.log(fullName)
+
+let a = 100; let b = '100'; let c = true;
+console.log(typeof a); console.log(typeof b); console.log(typeof c);
+
+let userName = prompt ("Введіть ваше імʼя")
+console.log("Привіт, " + userName + "!");
+
+let ageInput = prompt("Скільки вам років?");
+let age = parseInt(ageInput);
+console.log("Вам " + age + " років.");
+
+
