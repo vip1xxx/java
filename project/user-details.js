567fe9c5427f4b77298ab256f5ae6de08b36efe8
x <fedorenkomcbook@gmail.com>
2023-09-29 12:27:31 +0200

    init commit

--- project/user-details.js
+++ project/user-details.js
@@ -73,3 +73,88 @@ prevUserButton.addEventListener('click', () => {
 nextUserButton.addEventListener('click', () => {
     updateUserIndex(currentUserIndex + 1);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const urlParams = new URLSearchParams(window.location.search);
+let userId = urlParams.get('userId');
+
+
+// Отримуємо дані користувачів з API
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+        // Знаходимо індекс користувача за його ID
+        for (let i = 0; i < users.length; i++) {
+            if (users[i].id === parseInt(userId)) {
+                currentUserIndex = i;
+                break;
+            }
+        }
+
+        // Перевірка, чи користувача знайдено
+        if (currentUserIndex === -1) {
+            console.error('User not found');
+            return;
+        }
+
+        const userDetails = document.querySelector('.user-details');
+        const user = users[currentUserIndex];
+
+        userDetails.innerHTML = `
+            <h2>User Details</h2>
+            <p>ID: ${user.id}</p>
+            <p>Name: ${user.name}</p>
+            <p>Username: ${user.username}</p>
+            <p>Email: ${user.email}</p>
+            <p>Phone: ${user.phone}</p>
+            <p>Website: ${user.website}</p>
+            <p>Address: ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
+            <p>Company: ${user.company.name}, ${user.company.catchPhrase}, ${user.company.bs}</p>
+        `;
+    })
+    .catch(error => console.error('Error fetching user details:', error));
+
+
+
+showPostsButton.addEventListener('click', () => {
+    const userId = usersData[currentUserIndex].id;
+    // Отримуємо список постів поточного користувача з API
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+        .then(response => response.json())
+        .then(posts => {
+            postList.innerHTML = ''; // Очищаємо список постів
+
+            posts.forEach(post => {
+                const postBlock = document.createElement('div');
+                postBlock.classList.add('post');
+
+                const postId = document.createElement('p');
+                postId.textContent = `ID: ${post.id}`;
+
+                const postTitle = document.createElement('p');
+                postTitle.textContent = `Title: ${post.title}`;
+
+                const postBody = document.createElement('p');
+                postBody.textContent = `Body: ${post.body}`;
+
+                postBlock.appendChild(postId);
+                postBlock.appendChild(postTitle);
+                postBlock.appendChild(postBody);
+
+                postList.appendChild(postBlock);
+            });
+        })
+        .catch(error => console.error('Error fetching user posts:', error));
+});
